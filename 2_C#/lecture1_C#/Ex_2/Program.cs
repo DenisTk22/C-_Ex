90610add7fd528843de9616107b0a61a31486cfa
Denis Tkachev <allpost-s@ya.ru>
2022-11-29 20:00:17 +0300

    изменение второго примера

--- 2_C#/lecture1_C#/Ex_2/Program.cs
+++ 2_C#/lecture1_C#/Ex_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+string username2 = Console.ReadLine();
 Console.Write("Привет,  ");
-Console.Write(username);
+Console.Write(username2);
