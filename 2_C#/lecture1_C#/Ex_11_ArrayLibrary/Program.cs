ce3e7353472a4d6d16fb1980b5b7e75777367791
Denis Tkachev <allpost-s@ya.ru>
2022-11-30 21:08:55 +0300

    Отладка примера Ex_11

---
+++ 2_C#/lecture1_C#/Ex_11_ArrayLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index +1;
+        index++;
+    }
+}
+//void ничего не возвращает, не нужно использовать оператор return
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+// Нахождение элемента массива:
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+//определили массив из 10 элементов:
+int[] array = new int[10];
+//заполнение массива FillArray и его вывод PrintArray:
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
