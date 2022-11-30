6f4bf5ff83170d02b923c5049f77bf25f83cbfe7
Denis Tkachev <allpost-s@ya.ru>
2022-11-30 20:09:19 +0300

    второй вариант записа строк 19 - 22

--- 2_C#/lecture1_C#/Ex_8_function/Program.cs
+++ 2_C#/lecture1_C#/Ex_8_function/Program.cs
@@ -13,12 +13,19 @@ int a2 = 15;
 int b2 = 152;
 int c2 = 15;
 int a3 = 15;
-int b3 = 4;
+int b3 = 400;
 int c3 = 15;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+// строки с 19 по 22 можно заменить одной строкой:
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
 
 Console.WriteLine(max);
