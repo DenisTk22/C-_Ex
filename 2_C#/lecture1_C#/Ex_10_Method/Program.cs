74bb129dc3356bfcdda58518ea4b548a3b1912c0
Denis Tkachev <allpost-s@ya.ru>
2022-11-30 20:26:33 +0300

    Отладка примера Ex_10

---
+++ 2_C#/lecture1_C#/Ex_10_Method/Program.cs
@@ -0,0 +1,20 @@
+﻿// Поиск позиции нужного элемента в массиве
+int [] array = {1, 12, 31, 4, 18, 15, 16, 18};
+
+// n это количество элементов массива
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        //остановка на первом найденном элементе:
+        break;
+    }
+    //index = index =1; или:
+    index ++;
+}
