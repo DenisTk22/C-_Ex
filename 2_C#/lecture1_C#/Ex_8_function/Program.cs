daa47d4d5d72f22fc83180aa0f9f8c56e0f46ef2
Denis Tkachev <allpost-s@ya.ru>
2022-11-30 20:05:36 +0300

    Отладка Ex_8_function

---
+++ 2_C#/lecture1_C#/Ex_8_function/Program.cs
@@ -0,0 +1,24 @@
+﻿// Функция возьмет три число и найдет из них максимальное, потом следующие три числе и найдет из них макс и в конце максимальное из трех максимальных.
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 2;
+int b1 = 54;
+int c1 = 15;
+int a2 = 15;
+int b2 = 152;
+int c2 = 15;
+int a3 = 15;
+int b3 = 4;
+int c3 = 15;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
